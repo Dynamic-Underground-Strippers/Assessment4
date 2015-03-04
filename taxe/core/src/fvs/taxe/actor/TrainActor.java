e5343a2e42a9251f41530c52f082af2389b5802c
Michael <m1chael0@hotmail.co.uk>
2015-03-04 13:40:48 +0000

    Adds to TrainActor

--- taxe/core/src/fvs/taxe/actor/TrainActor.java
+++ taxe/core/src/fvs/taxe/actor/TrainActor.java
@@ -5,10 +5,15 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import fvs.taxe.controller.Context;
 import gameLogic.Game;
 import gameLogic.GameState;
+import gameLogic.Player;
 import gameLogic.map.IPositionable;
+import gameLogic.map.Station;
 import gameLogic.resource.Train;
+
+import java.awt.*;
 
 /**This class is a type of image specifically for creating Actors for Trains.*/
 public class TrainActor extends Image {
@@ -36,6 +41,14 @@ public class TrainActor extends Image {
     /**The default drawable for the train's right image.*/
     private Drawable rightDrawable;
 
+    /**Indicates whether the train is paused at a blocked connection or not.*/
+    private boolean paused;
+
+    /**Indicates whether the train has recently been paused at a blocked connection.*/
+    private boolean recentlyPaused;
+
+    private Context context;
+
     /**The instantiation method sets up the drawables and bounds and positions the TrainActor.
      * @param train The train to base the TrainActor off.
      */
@@ -53,15 +66,49 @@ public class TrainActor extends Image {
         setPosition(position.getX() - width / 2, position.getY() - height / 2);
         previousX = getX();
         facingLeft = true;
+        paused = false;
+        recentlyPaused = false;
     }
 
     @Override
     public void act (float delta) {
-        if (Game.getInstance().getState() == GameState.ANIMATING) {
+        if ((Game.getInstance().getState() == GameState.ANIMATING) && (!this.paused)) {
             super.act(delta);
             updateBounds();
             updateFacingDirection();
+
+            Train collision = collided();
+            if (collision != null) {
+                //If there is a collision then the user is informed, the two trains destroyed and the connection that they collided on is blocked for 5 turns.
+                context.getTopBarController().displayFlashMessage("Two trains collided.  They were both destroyed.", Color.RED, 2);
+                Game.getInstance().getMap().blockConnection(train.getLastStation(), train.getNextStation(), 5);
+                collision.getActor().remove();
+                collision.getPlayer().removeResource(collision);
+                train.getPlayer().removeResource(train);
+                this.remove();
+            }
+
+        } else if (this.paused) {
+            //Everything inside this block ensures that the train does not move if the paused variable is set to true.
+            //This ensures that trains do not move through blocked connections when they are not supposed to.
+
+            //find station train most recently passed
+            Station station = train.getHistory().get(train.getHistory().size() - 1).getFirst();
+//            Station station = Game.getInstance().getMap().getStationByName(stationName);
+
+            // find index of this within route
+            int index = train.getRoute().indexOf(station);
+
+            // find next station
+            Station nextStation = train.getRoute().get(index + 1);
+
+            // check if connection is blocked, if not, unpause
+            if (!Game.getInstance().getMap().isConnectionBlocked(station, nextStation)) {
+                this.paused = false;
+                this.recentlyPaused = true;
+            }
         }
+
     }
 
     /**Refreshes the bounds of the actor*/
@@ -87,5 +134,46 @@ public class TrainActor extends Image {
     /**@returns the bounds of the TrainActor*/
     public Rectangle getBounds() {
         return bounds;
+    }
+
+    public Train collided() {
+        //The aim of this function is to check whether the train represented by the actor has collided with any other trains on the board
+        Station last = train.getLastStation();
+        Station next = train.getNextStation();
+        if (train.getPosition().getX() == -1 && !paused) {
+            //if this train is moving;
+            for (Player player : Game.getInstance().getPlayerManager().getAllPlayers()) {
+                for (Train otherTrain : player.getTrains()) {
+                    //This checks every train that is currently present within the game
+                    if (!otherTrain.equals(train)) {
+                        //don't check if collided with self
+                        if (otherTrain.getPosition() != null) {
+                            //Checks if the other train has been placed on the map
+                            if (otherTrain.getPosition().getX() == -1 && !otherTrain.getActor().getPaused()) {
+                                //if other train moving
+                                //This is because the position of the train when it is in motion (i.e travelling along its route) is (-1,-1) as that is how FVS decided to implement it
+                                //It is necessary to check whether this is true as if the train is not in motion then it does not have an actor, hence otherTrain.getActor() would cause a null point exception.
+
+                                if ((otherTrain.getNextStation() == next && otherTrain.getLastStation() == last)
+                                        || (otherTrain.getNextStation() == last && otherTrain.getLastStation() == next)) {
+                                    //check if trains on same connection
+
+
+                                    if ((this.bounds.overlaps(otherTrain.getActor().getBounds())) && !((this.recentlyPaused) || (otherTrain.getActor().isRecentlyPaused()))) {
+                                        //Checks whether the two trains are recently paused, if either of them are then no collision should occur
+                                        //This prevents the issue of two paused trains crashing when they shouldn't
+                                        //There is still the potential issue of two blocked trains colliding when they shouldn't, as it is impossible to know which connection a blocked train will occupy. i.e when one train is rerouted but not the other
+                                        return otherTrain;
+                                        //This is slightly limiting as it only allows two trains to collide with each other, whereas in theory more than 2 could collide, this is however very unlikely and due to complications
+                                        //not necessary to factor in to our implementation at this stage. If you need to add more trains then you would have to build up a list of collided trains and then return it.
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return null;
     }
 }
