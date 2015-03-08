1553d9ba551fa87e2edea677c429a9de7d34ef2e
Michael <m1chael0@hotmail.co.uk>
2015-03-08 20:11:05 +0000

    New Connection class

---
+++ taxe/core/src/gameLogic/resource/NewConnection.java
@@ -0,0 +1,42 @@
+package gameLogic.resource;
+
+import gameLogic.map.Connection;
+import gameLogic.map.Station;
+
+
+public class NewConnection extends Resource {
+    
+    private Station station1;
+    private Station station2;
+
+    public NewConnection() {
+        this.name = "New Connection";
+        //By default forTurns set to 5, possible to make this random instead
+        this.forTurns = 5;
+        this.station1 = null;
+        this.station2 = null;
+
+    }
+
+
+    public Station getStation1() {
+        return station1;
+    }
+
+    public void setStation1(Station station1) {
+        this.station1 = station1;
+    }
+
+    public Station getStation2() {
+        return station2;
+    }
+
+    public void setStation2(Station station2) {
+        this.station2 = station2;
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
